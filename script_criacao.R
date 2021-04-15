a47416a36ba891144a8d9fe46c06b70f7cb83b02
Leandro25-ap <leandroalvespinto96@gmail.com>
2021-04-14 21:42:30 -0300

    Primeiro

--- script_criacao.R
+++ script_criacao.R
@@ -1,9 +1,9 @@
-
+#usethis::create_project("c:\\projetosR\\lab_ciecia_dados")
 
 #Criar a estrutura GIT para esse projeto
 usethis::use_git()
 usethis::use_github()
 usethis::create_github_token()
 
-gitcreds::gitcreds_set(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)
+gitcreds::gitcreds_set()
 
